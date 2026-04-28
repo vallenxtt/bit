2ecce4a9a15942cffaba5903fdfe9a7757eefbf0
filipgay <dant1k.omg@gmail.com>
2026-04-28 13:18:53 +0200

    koddziala

--- zadanie/main.cpp
+++ zadanie/main.cpp
@@ -1,9 +1,27 @@
 #include <iostream>
+#include <cstdint>
 
 using namespace std;
 
-int main()
-{
-    cout << "Hello world!" << endl;
+int getbit(uint8_t a, int pos) {
+    return (a >> pos) & 1;
+}
+
+void setbit(uint8_t &a, int pos, int val) {
+    if (val == 1) {
+        a |= (1 << pos);
+    } else {
+        a &= ~(1 << pos);
+    }
+}
+
+int main() {
+    uint8_t a = 4;
+
+    setbit(a, 0, 1);
+
+    cout << getbit(a, 0) << endl;
+    cout << (int)a << endl;
+
     return 0;
 }
